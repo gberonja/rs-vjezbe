8f3af3ad81943452f49f8e1fac2add66e1489953
Gabriel Beronja <124529968+gberonja@users.noreply.github.com>
2024-11-13 22:04:34 +0100

    Ispravka rješenja
    Ispravka zadataka

--- RS-1-zadaće/zadatak_4.py
+++ RS-1-zadaće/zadatak_4.py
@@ -1,7 +1,7 @@
 broj = 0
 while broj < 5:
     broj +=2
-    print(broj) # ispisati će brojeve 2 i 4
+    print(broj) # ispisati će brojeve 2, 4 i 6
     
 broj = 0
 while broj < 5:
@@ -9,7 +9,7 @@ while broj < 5:
     print(broj)
     broj -= 1
     
-# svaki put kada broj prolazi petlju, poveća mu se vrijednost za 1 i smanji za 1
+# svaki put kada broj prolazi petlju, poveća mu se vrijednost za 1 i smanji za 1; beskonačna petlja
 
 broj = 10
 while broj > 0:
@@ -18,5 +18,5 @@ while broj > 0:
     if broj < 5:
         broj += 2
         
-# petlja će funkcionirati do broja 4, onda će se stalno mijenjati vrijednost 6,5,4
+# petlja će funkcionirati do broja 4, onda će se stalno mijenjati vrijednost 6,5,4, u ispisu 5,4
     
